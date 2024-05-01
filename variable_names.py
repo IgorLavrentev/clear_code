e7806e73bd4c505e71be3eafa73c99830ef88a09
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-05-01 23:13:03 +0300

    add variable_names.py

---
+++ variable_names.py
@@ -0,0 +1,35 @@
+decimal_number - decimal_number_system
+# переменная, хранящая число в десятичной системе счисления
+
+final_list - result_ list_decimal_system
+# результирующий массив чисел в десятичной системе счисления
+
+summ - sum_all_numbers_except_following
+# сумма всех чисел, кроме следующего
+
+x - next_item_in_list
+# переменная для следующего элемента в списке
+
+list_number - list_sales_values
+# список значений продаж
+
+items_new - grouped_sales_summary
+# сводка по продажам в сгруппированном виде
+
+string - final_magic_word
+# итоговое магическое слово (слово, полученное при помощи определённых преобразований)
+
+reverse - reverse_order_ array_elements
+# обратный порядок элементов массива
+
+list_f - list_strings_certain_length
+# массив строк определенной длины после разбивки исходной строки
+
+list_ret - final_list_zeros_ones
+# массив целых чисел, содержащий 1 или 0 (есть ли слово в соответствующей строке)
+
+doors_array - list_open_and_closed_doors
+# массив открытых и закрытых дверей
+
+s - index_ middle_list
+# индекс середины списка
