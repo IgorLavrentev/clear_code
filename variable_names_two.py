4619066dfd0e9c28fe74742498f84567fc744108
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-05-03 23:09:49 +0300

    add variable_names_two.py

---
+++ variable_names_two.py
@@ -0,0 +1,40 @@
+# 6.1. Уровни абстракции в именах переменных
+initial_list_1 - list_of_tree
+# список веток деревьев
+time - travel_time
+# время в пути
+summ – number_of_matches
+# переменная для подсчета количества совпадений
+samm – samm_maximum_discount
+# переменная для подсчета суммы максимальной скидки
+list_percentages - list_of_percentages_for_each_candidate
+# количество процентов для каждого кандидата
+
+# 6.2. Технические термины в качестве имён переменных
+slice – slice_of_string
+# часть строки
+original_value
+# значение исходного слова
+dictionary_1 - dictionary_of_repetition_counts
+# словарь количеств повторений
+decimal_number
+# десятичное число
+# 6.3. Имена переменных с учётом контекста
+doors_array
+# создание массива открытых и закрытых дверей
+distance
+# общая протяженность поездки
+field - training_ground_squares
+# тренировочный полигон "Квадраты"
+
+# 6.4. Переменные длина которых укладываются в 8-20 символов 
+m – size_of_submatrix
+# размер подматрицы
+result – size_of_matrix
+# размер матрицы
+s_1 – row_matrix
+# строка матрицы
+s_11 – part_of_matrix_row
+# часть строки матрицы
+s_111 – submatrix
+# подматрица
