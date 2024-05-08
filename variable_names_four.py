ca619bdb754945153b1360d75f04fc5ac6b452bd
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-05-08 22:31:30 +0300

    add variable_names_four.py

---
+++ variable_names_four.py
@@ -0,0 +1,50 @@
+for j in range(len(data)):
+    summ += data[j]
+summ - numbers_sum
+# переменная для суммы всех чисел, кроме следующего
+
+for i in range(N):
+    for j in range(N):
+        el_1 = items[i]
+        el_2 = items[j]
+el_1 - element_of_first_pass
+el_2 - element_of_second_pass
+# элементы первого и второго прохода по списку
+
+n_1 - first_index_of_gap
+n_2 - second_index_of_gap
+# индексы пробела в строке
+
+list_number - sales_values_list
+# список занчений продаж
+
+summ_el: int = 0 - ordered_elements_sum
+# сумма элементов следующий из которых больше текущего
+
+list_f - set_of_rows
+# массив для получения набора строк
+
+list_ret - presence_of_words
+# массив с признаками нахождения слова в соответствующей строке
+
+string - open_closed_doors
+# строка открытых и закрытых дверей
+
+distance - distance_km
+# общая протяженность поездки
+
+str_1 - top_row
+# верхняя строка
+str_2 - right_row
+# правая строка
+str_3 - bottom_row
+# нижняя строка
+str_4 - left_row
+# левая строка
+
+list_changes - changeable_list
+# список в который будут записываться все изменения строки
+
+summ_1 - stars_number
+summ_2 - points_number
+# сумма звёздочек, сумма точек
