b628a00f4dbbc6c0081ed691dc247d77186ba9bb
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-05-15 13:33:10 +0300

    add function_names.py

---
+++ function_names.py
@@ -0,0 +1,36 @@
+get_car_rims - print_car_rims_radius
+# напечатать радиус диска автомобиля
+
+speed - change_speed
+# изменение скорости
+
+get_car_speed - print_car_speed
+# напечатать скорость автомобиля
+
+ABS - enabling_and_disabling_ABS
+# включение и отключение антиблокировочной системы
+
+roll_angle_new - сhange_roll_angle
+# изменение угла крена
+
+get_roll_angle - print_roll_angle
+# напечатать угол крена
+
+oxygen_supply - supply_oxygen
+# подать кислород (определенное значение)
+
+driving_style - сhange_driving_style
+# изменить стиль вождения
+
+engine - print_engine_brand
+# печать марки двигателя
+
+orbit_correction - changing_orbit
+# коррекция орбиты
+
+medicine_chest_plus - аpply_medicine_chest
+# применить аптечку
+
+medicine_chest_minus - dealing_damage
+# нанесение урона
+
