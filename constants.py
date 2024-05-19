b8c8599fdddc1a5589ca9386d8002569eb2145ad
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-05-19 23:16:42 +0300

    add constants.py

---
+++ constants.py
@@ -0,0 +1,65 @@
+one_percent = 100/sum(Votes) - one_percent = PERCENTAGES/sum(Votes)
+PERCENTAGES = 100
+# замена численного значения значением константы, использование только заглавных букв
+
+n = 100 # размер массива
+n = 100 – ARRAY_SIZE = 100
+for i in range(ARRAY_SIZE)
+# использование только заглавных букв
+
+PASSAGES_THROUGH_LIST = 5 # количество проходов по списку
+for i in range(PASSAGES_THROUGH_LIST):
+# введение отдельной переменной для константы, использование только заглавных букв
+
+pi = float(input()) 
+print(pi > 3.1405 and pi < 3.1423) # проверяем лежит ли вещественная переменная pi в диапазоне от 3.1405 до 3.1423
+left_border_PI = 3.1405
+right_border_PI = 3.1423
+# введение отдельных переменных для левой и правой границы значений PI, использование только заглавных букв
+
+r = int(input("Радиус окружности: "))
+PI = 3.14
+print("Периметр окружности:", 2 * PI * r)
+# введение отдельной переменной, использование только заглавных букв
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+alphabet_2 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ENGLISH_ALPHABET_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
+ENGLISH_ALPHABET_UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# переименование переменных, использование только заглавных букв
+
+months_ru = ['', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+months_en = ['', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+MONTHS_IN_RUSSIAN = ['', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']    
+MONTHS_IN_ENGLISH= ['', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+# переименование переменных, использование только заглавных букв
+
+if text[i] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ENGLISH_ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+if text[i] in ENGLISH_ALPHABET
+# введение отдельной переменной, использование только заглавных букв
+
+answers = ['Бесспорно', 'Мне кажется - да', 'Пока не ясно, попробуй снова', 'Даже не думай', 'Предрешено', 'Вероятнее всего']
+RANDOM_ANSWERS = ['Бесспорно', 'Мне кажется - да', 'Пока не ясно, попробуй снова', 'Даже не думай', 'Предрешено', 'Вероятнее всего']
+# переименование переменной, использование только заглавных букв
+
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+DIGITS = '0123456789'
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PUNCTUATION = '!#$%&*+-=?@^_'
+# использование только заглавных букв
+
+word_list = ['математика', 'геометрия', 'информатика', 'программирование', 'питон', 'образование', 'телефон']
+WORD_LIST = ['математика', 'геометрия', 'информатика', 'программирование', 'питон', 'образование', 'телефон']
+# использование только заглавных букв
+
+months_ru = ['', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']    
+months_en = ['', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+
+MONTHS_IN_RUSSIAN = ['', 'январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']    
+MONTHS_IN_ENGLISH = ['', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+# переименование переменных, использование только заглавных букв
