4732983992aac1d7783801dcf2f3a9222bf9cfa6
IgorLavrentev <igor.lavrentev.bmstu@gmail.com>
2024-05-22 23:08:16 +0300

    add types_of_data.py

---
+++ types_of_data.py
@@ -0,0 +1,117 @@
+for i in range(M, N + 1): 
+    sum += i
+average_value = sum / (N + 1 - M) # вычисляем среднее значение
+print("Сумма чисел равна", sum)
+print("Среднее значение равно", average_value)
+
+for i in range(M, N + 1): 
+    sum += i
+try:
+    average_value = sum / (N + 1 - M)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+else:
+    average_value = sum / (N + 1 - M) # вычисляем среднее значение
+    print("Сумма чисел равна", sum)
+    print("Среднее значение равно", average_value)
+# добавлено исключение ошибки деления на ноль
+
+
+for i in range(100): # заполняем словарь: 100 случайных пар ключ + строка
+    dictionary_one[list_one[i]] = random.choice(verbs)
+number_of_words = 100
+
+for i in range(100): # заполняем словарь: 100 случайных пар ключ + строка
+    dictionary_one[list_one[i]] = random.choice(verbs)
+# добавление константы
+
+
+if str(v) == el[n + 1:]: - if str(value_of_sales) == el[n + 1:]:
+# преобразования типов приведено в более понятный вид
+
+
+one_percent = 100/sum(Votes)
+
+try:
+    one_percent = 100/sum(Votes)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+    return
+else:
+    one_percent = 100/sum(Votes)
+# добавлено исключение ошибки деления на ноль
+
+
+if el in ['0','1','2','3','4','5','6','7','8','9']:
+
+array_of_up_ten = ['0','1','2','3','4','5','6','7','8','9']:
+if el in array_of_up_ten:
+# добавление константы
+
+
+if 1 <= num <= 100:
+
+final_number = 100
+if 1 <= num <= final_number:
+# добавление константы
+
+
+t = s / (v1 + v2)
+
+try:
+    t = s / (v1 + v2)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+else:
+    t = s / (v1 + v2)
+# добавлено исключение ошибки деления на ноль
+
+
+    x1 = 1 / x
+
+try:
+    x1 = 1 / x
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+else:
+    x1 = 1 / x
+# добавлено исключение ошибки деления на ноль
+
+if line[i] in ['0','1','2','3','4','5','6','7','8','9']:
+
+array_of_up_ten = ['0','1','2','3','4','5','6','7','8','9']
+if line[i] in array_of_up_ten:
+# добавление константы
+
+
+result = a/b
+
+try:
+    result = a/b
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+else:
+    result = a/b
+# добавлено исключение ошибки деления на ноль
+
+
+price = int(product['цена'] * (1.0 — discount))
+
+try:
+    price = int(product['цена'] * (1.0 — discount))
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+else:
+    price = int(product['цена'] * (1.0 — discount))
+# добавлено исключение ошибки деления на ноль
+
+
+print(int(a) / int(b))
+
+try:
+    int(a) / int(b)
+except ZeroDivisionError:
+    print("ZeroDivisionError")
+else:
+    print(int(a) / int(b))
+# добавлено исключение ошибки деления на ноль
